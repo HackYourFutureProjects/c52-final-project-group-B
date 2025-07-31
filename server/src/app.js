f0f935c8a50a24efeac11bb3755a35433222c0de
A. Zetouny <Zet_Ahmad@hotmail.com>
2025-07-31 21:58:48 +0200

    Fix #2

--- server/src/app.js
+++ server/src/app.js
@@ -1,15 +1,32 @@
 import express from "express";
 import cardRouter from "./cards/card.router.js";
-// import { errorHandler } from "./middlewares/errorHandler.middleware.js";
-// import { notFound } from "./middlewares/notFound.middleware.js";
+import { errorHandler } from "./middlewares/errorHandler.middleware.js";
+import { notFound } from "./middlewares/notFound.middleware.js";
 
 // Create an express server
 const app = express();
 
 app.use(express.json());
 
+/****** Host our client code for Heroku *****/
+/**
+ * We only want to host our client code when in production mode as we then want to use the production build that is built in the dist folder.
+ * When not in production, don't host the files, but the development version of the app can connect to the backend itself.
+ */
+if (process.env.NODE_ENV === "production") {
+  app.use(
+    express.static(new URL("../../client/dist", import.meta.url).pathname),
+  );
+  // Redirect * requests to give the client data
+  app.get("/*file", (req, res) =>
+    res.sendFile(
+      new URL("../../client/dist/index.html", import.meta.url).pathname,
+    ),
+  );
+}
+
 app.use("/api/cards", cardRouter);
-// app.use(notFound);
-// app.use(errorHandler);
+app.use(notFound);
+app.use(errorHandler);
 
 export default app;
