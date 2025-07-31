5e68da5d3e4901813adc5456d7296bd8a97dff14
yanaesher <senyukyana26@gmail.com>
2025-07-31 19:29:46 +0200

    Add shared folder for shared dep

--- server/src/app.js
+++ server/src/app.js
@@ -6,14 +6,8 @@ import { notFound } from "./middlewares/notFound.middleware.js";
 // Create an express server
 const app = express();
 
-// Tell express to use the json middleware
 app.use(express.json());
 
-/****** Attach routes ******/
-/**
- * We use /api/ at the start of every route!
- * As we also host our client code on heroku we want to separate the API endpoints.
- */
 app.use("/api/cards", cardRouter);
 app.use(notFound);
 app.use(errorHandler);
