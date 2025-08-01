ed462ea0ea0e38262e300cb4306faf5a0fe0bf21
yanaesher <senyukyana26@gmail.com>
2025-08-01 19:07:33 +0200

    run prettier

--- server/src/app.js
+++ server/src/app.js
@@ -9,25 +9,23 @@ const app = express();
 
 app.use(express.json());
 
-/****** Host our client code for Heroku *****/
-/**
- * We only want to host our client code when in production mode as we then want to use the production build that is built in the dist folder.
- * When not in production, don't host the files, but the development version of the app can connect to the backend itself.
- */
+app.use("/api/cards", cardRouter);
+app.use("/api/decks", deckRouter);
+
 if (process.env.NODE_ENV === "production") {
   app.use(
     express.static(new URL("../../client/dist", import.meta.url).pathname),
   );
-  // Redirect * requests to give the client data
-  app.get("/*file", (req, res) =>
+
+  app.get("*", (req, res, next) => {
+    if (req.path.startsWith("/api")) return next();
+
     res.sendFile(
       new URL("../../client/dist/index.html", import.meta.url).pathname,
-    ),
-  );
+    );
+  });
 }
 
-app.use("/api/cards", cardRouter);
-app.use("/api", deckRouter);
 app.use(notFound);
 app.use(errorHandler);
 
