d3c5b297391cfc9e7ef0a02832cd17c2de0e626b
Nikita Stefanchuk <nik.stefanchuk@gmail.com>
2025-08-02 19:17:18 +0200

    deck and cards connected API to front v6

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,29 +1,21 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ command, mode }) => {
+export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "VITE_");
   const backendProxyTarget = env.VITE_BACKEND_URL ?? "http://localhost:3000";
+
   return {
-    plugins: [react(), tailwindcss()],
+    plugins: [react()],
     resolve: {
       alias: {
         "@": path.resolve(__dirname, "./src"),
       },
     },
     server: {
-      // Specify server port. Note if the port is already being used,
-      // Vite will automatically try the next available port so this may not be the actual port
-      // the server ends up listening on.
       port: 5173,
-
-      // Automatically open the app in the browser on server start.
       open: "/",
-
-      // Proxy /api requests to the API server. This will avoid any CORS issues.
       proxy: {
         "/api": backendProxyTarget,
       },
