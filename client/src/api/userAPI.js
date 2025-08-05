aa52ef7d4c9b36ed17bc0b06a913d3a5002e2382
Nikita Stefanchuk <nik.stefanchuk@gmail.com>
2025-08-05 22:40:13 +0200

    feature/result-page-and-backend-user-progress v1

--- client/src/api/userAPI.js
+++ client/src/api/userAPI.js
@@ -2,7 +2,7 @@ import apiRequest from "./index.js";
 
 export const submitUserProgress = async (body) => {
   try {
-    const res = await apiRequest(`/submit`, "POST", body);
+    const res = await apiRequest(`/user-progress/submit`, "POST", body);
     return res;
   } catch (e) {
     console.error(e);
