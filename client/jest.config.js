09353c65c0a29c6d4b023a40e4bd5d7b8801c065
A. Zetouny <Zet_Ahmad@hotmail.com>
2025-09-01 14:59:07 +0200

    Removed unused file/test files from client

--- client/jest.config.js
+++
@@ -1,8 +0,0 @@
-export default {
-  testEnvironment: "jsdom",
-  setupFilesAfterEnv: ["./setupTests.js"],
-  moduleNameMapper: {
-    "\\.(css|less)$": "<rootDir>/__mocks__/styleMock.js",
-    "\\.(jpg|jpeg|png|svg)$": "<rootDir>/__mocks__/fileMock.js",
-  },
-};
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,8 +15,6 @@ import BrowseDecks from "@/pages/BrowseDecks";
 import MyDecks from "@/pages/MyDecks";
 import { ROUTES } from "@/routes/paths";
 import Support from "@/pages/Support";
-import GenerateDeck from "@/pages/GenerateDeck";
-import CreateDeck_V2 from "@/pages/CreateDeck_v2";
 
 const App = () => {
   return (
@@ -37,9 +35,7 @@ const App = () => {
       <Route path={ROUTES.DECKS} element={<BrowseDecks />} />
       <Route path={ROUTES.BROWSE} element={<BrowseDecks />} />
       <Route path={ROUTES.MY_DECKS} element={<MyDecks />} />
-      <Route path={ROUTES.DECK_GENERATE} element={<GenerateDeck />} />
       <Route path={ROUTES.DECK_CREATE} element={<CreateDeck />} />
-      <Route path="/decks/create_v2" element={<CreateDeck_V2 />} />
       <Route path={ROUTES.DECK_DETAILS(":id")} element={<DeckPage />} />
       <Route path={ROUTES.DECK_EDIT(":id")} element={<EditDeck />} />
       <Route path={ROUTES.DECK_CARD_MODE(":id")} element={<CardMode />} />
